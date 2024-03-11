2af1a3c243e13198b38282aa7fcd971a91bfed56
Abe M <abe.malla8@gmail.com>
2024-03-10 22:16:56 -0700

    Finish buffer

--- src/utilities/memory/buffer.rs
+++ src/utilities/memory/buffer.rs
@@ -1,11 +1,14 @@
 use std::ops::{Deref, DerefMut, Index, IndexMut};
-use std::{marker::PhantomData, mem, ptr};
+use std::{
+    marker::PhantomData,
+    ptr::{copy_nonoverlapping, write_bytes, NonNull},
+};
 
 use crate::utilities::memory::unmanaged_mempool::IUnmanagedMemoryPool;
 
 /// Represents a span of unmanaged memory.
 pub struct Buffer<T> {
-    memory: ptr::NonNull<T>,
+    memory: NonNull<T>,
     length: usize,
     id: i32,
     /// PhantomData is used to indicate that the Buffer<T> struct is logically owning data of type T.
@@ -14,9 +17,9 @@ pub struct Buffer<T> {
 
 impl<T: std::cmp::PartialEq> Buffer<T> {
     /// Creates a new buffer.
-    pub fn new(memory: *mut T, length: usize, id: i32) -> Self {
+    pub unsafe fn new(memory: *mut T, length: usize, id: i32) -> Self {
         Buffer {
-            memory,
+            memory: NonNull::new(memory).expect("Null pointer passed to Buffer::new"),
             length,
             id,
             _marker: PhantomData,
@@ -24,10 +27,7 @@ impl<T: std::cmp::PartialEq> Buffer<T> {
     }
 
     /// Allocates a new buffer from a pool.
-    pub fn from_pool(
-        length: usize,
-        pool: &mut impl IUnmanagedMemoryPool,
-    ) -> Result<Self, &'static str> {
+    pub fn from_pool(length: usize, pool: &mut impl IUnmanagedMemoryPool) -> Option<Self> {
         pool.take(length)
     }
 
@@ -37,6 +37,7 @@ impl<T: std::cmp::PartialEq> Buffer<T> {
     }
 
     /// Gets whether the buffer references non-null memory.
+    #[inline]
     pub fn is_allocated(&self) -> bool {
         !self.memory.as_ptr().is_null()
     }
@@ -47,51 +48,62 @@ impl<T: std::cmp::PartialEq> Buffer<T> {
     }
 
     /// Zeroes out the buffer's memory.
+    #[inline]
     pub fn clear(&mut self, start: usize, count: usize) {
+        #[cfg(debug_assertions)]
         debug_assert!(start + count <= self.length, "Clear region out of bounds.");
         unsafe {
-            ptr::write_bytes(
-                self.memory.as_ptr().add(start),
-                0,
-                count * mem::size_of::<T>(),
-            );
+            write_bytes::<T>(self.memory.as_ptr().add(start), 0, count);
         }
     }
 
     /// Creates a view of a subset of the buffer's memory.
+    #[inline]
     pub fn slice(&self, start: usize, count: usize) -> Self {
+        #[cfg(debug_assertions)]
         debug_assert!(start + count <= self.length, "Slice region out of bounds.");
-        let new_memory = unsafe { self.memory.as_ptr().add(start) };
-        Buffer {
-            memory: new_memory,
-            length: count,
-            id: self.id,
-            _marker: PhantomData,
+        unsafe {
+            Buffer::new(
+                NonNull::new_unchecked(self.memory.as_ptr().add(start)),
+                count,
+                self.id,
+            )
         }
     }
 
+    /// Creates a view of a subset of the buffer's memory, starting from the first index.
+    #[inline]
+    pub fn slice_from_start(&self, count: usize) -> Self {
+        self.slice(0, count)
+    }
+
     /// Copies buffer data into another buffer.
-    pub fn copy_to(
+    pub unsafe fn copy_to(
         &self,
         source_start: usize,
         target: &mut Buffer<T>,
         target_start: usize,
         count: usize,
-    ) {
-        debug_assert!(
-            source_start + count <= self.length,
-            "Source region out of bounds"
-        );
-        debug_assert!(
-            target_start + count <= target.length,
-            "Target region out of bounds"
-        );
+    ) where
+        T: Copy,
+    {
+        #[cfg(debug_assertions)]
+        {
+            debug_assert!(
+                source_start + count <= self.length,
+                "Source region out of bounds"
+            );
+            debug_assert!(
+                target_start + count <= target.length,
+                "Target region out of bounds"
+            );
+        }
 
         unsafe {
-            ptr::copy_nonoverlapping(
+            copy_nonoverlapping::<T>(
                 self.memory.as_ptr().add(source_start),
                 target.memory.as_ptr().add(target_start),
-                count * mem::size_of::<T>(),
+                count,
             );
         }
     }
@@ -104,26 +116,30 @@ impl<T: std::cmp::PartialEq> Buffer<T> {
         target_start: usize,
         count: usize,
     ) {
-        debug_assert!(
-            source_start + count <= source.len(),
-            "Source region out of bounds"
-        );
-        debug_assert!(
-            target_start + count <= self.length,
-            "Target region out of bounds"
-        );
+        #[cfg(debug_assertions)]
+        {
+            debug_assert!(
+                source_start + count <= source.len(),
+                "Source region out of bounds"
+            );
+            debug_assert!(
+                target_start + count <= self.length,
+                "Target region out of bounds"
+            );
+        }
 
         unsafe {
-            ptr::copy_nonoverlapping(
+            copy_nonoverlapping::<T>(
                 source.as_ptr().add(source_start),
                 self.memory.as_ptr().add(target_start),
-                count * mem::size_of::<T>(),
+                count,
             );
         }
     }
 
     /// Gets the index of an element, if it exists, using the type's default equality comparison.
     pub fn index_of(&self, element: &T, start: usize, count: usize) -> Option<usize> {
+        #[cfg(debug_assertions)]
         debug_assert!(start + count <= self.length, "Index region out of bounds");
 
         for i in start..(start + count) {
@@ -133,19 +149,17 @@ impl<T: std::cmp::PartialEq> Buffer<T> {
         }
         None
     }
+}
 
+impl<T: Copy> Buffer<T> {
     /// Creates a typed region from the raw buffer with the largest capacity that can fit within the allocated bytes.
+    /// This is a safe operation only if `T` and `TCast` have compatible memory layouts and alignment requirements.
     pub fn as_cast<TCast>(&self) -> Buffer<TCast>
     where
         TCast: Sized,
     {
-        let count = self.length * mem::size_of::<T>() / mem::size_of::<TCast>();
-        Buffer {
-            memory: self.memory as *mut TCast,
-            length: count,
-            id: self.id,
-            _marker: PhantomData,
-        }
+        let new_length = (self.length * std::mem::size_of::<T>()) / std::mem::size_of::<TCast>();
+        unsafe { Buffer::new(self.memory.cast(), new_length, self.id) }
     }
 }
 
@@ -154,6 +168,7 @@ impl<T> Index<usize> for Buffer<T> {
     type Output = T;
 
     fn index(&self, index: usize) -> &Self::Output {
+        #[cfg(debug_assertions)]
         debug_assert!(index < self.length);
         unsafe { &*self.memory.as_ptr().add(index) }
     }
@@ -161,6 +176,7 @@ impl<T> Index<usize> for Buffer<T> {
 
 impl<T> IndexMut<usize> for Buffer<T> {
     fn index_mut(&mut self, index: usize) -> &mut Self::Output {
+        #[cfg(debug_assertions)]
         debug_assert!(index < self.length);
         unsafe { &mut *self.memory.as_ptr().add(index) }
     }
